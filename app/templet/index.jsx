b6810923d14a036b69af7d96bab8fd404ac978b3
Ming Yang <yang.ming@rytong.com>
2017-12-04 08:55:15 +0800

    调整

--- app/templet/index.jsx
+++ app/templet/index.jsx
@@ -4,23 +4,23 @@ import Button from 'base/button';
 import Input from 'base/input';
 import '../style/index.less';
 
-class App extends React.Component{
-    constructor() {
-        super();
-    }
-    render() {
-        //JSX here!
-        return (
-          <div className="container">
-            <section className="jumbotron">
-              <h2 className="test">112Search Github Users</h2>
-              <Button/>122211111111
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    //JSX here!
+    return (
+      <div className="container">
+        <section className="jumbotron">
+          <h2 className="test">112Search Github Users</h2>
+          <Button />122211111111
               <Input />
 
-            </section>
-          </div>
-        )
-    }
+        </section>
+      </div>
+    )
+  }
 };
 
 const app = document.createElement('div');
